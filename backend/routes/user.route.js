fc0ff026d9c505472fe3efcadfe7029937d060cd
Arsalaan N <arsalaannaeem@hotmail.com>
2024-04-04 02:31:29 +0100

    updated README.md & auth routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -5,9 +5,9 @@ const { requireAuth } = require("../middleware/auth.middleware");
 const router = express.Router();
 
 // USER / AUTH ROUTES
-router.post("/auth/signup", userController.signup);
-router.post("/auth/login", userController.login);
-router.get("/auth/logout", userController.logout);
-router.get("/auth/check-auth", requireAuth, userController.checkAuth);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/logout", userController.logout);
+router.get("/check-auth", requireAuth, userController.checkAuth);
 
 module.exports = router;
